39c41f1f6051c5cb7343991f1857b854d73ea95a
James Peach <jpeach@apache.org>
2018-01-02 21:49:36 -0800

    Exercise 1.11.
    Update exercise 1.11 to automatically pull the Alexa top 1m sites
    dataset and fetch a random subset of those sites (over HTTP).

--- 1.11/fetchall.go
+++ 1.11/fetchall.go
@@ -8,29 +8,119 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
-	"os"
+	"net/url"
 	"time"
 )
 
+var fetchCount = flag.Int("count", 10, "number of random sites to fetch")
+var randSeed = flag.Int64("seed", time.Now().Unix(), "random seed")
+
 func main() {
+	flag.Parse()
+
+	log.Printf("Fetching top 1m sites")
+
+	sites, err := FetchTopSites()
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	log.Printf("Done")
+
+	rand.Seed(*randSeed)
+
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := range os.Args[1:] {
+
+	log.Printf("fetching %d URLs", *fetchCount)
+
+	for i := 0; i < *fetchCount; i++ {
+		url := sites[rand.Intn(len(sites))]
 		go fetch(url, ch) // start a goroutine
 	}
-	for range os.Args[1:] {
+
+	for i := 0; i < *fetchCount; i++ {
 		fmt.Println(<-ch) // receive from channel ch
 	}
+
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
-func fetch(url string, ch chan<- string) {
+// FetchTopSites returns the top 1M dataset from Alexa.
+func FetchTopSites() ([]string, error) {
+	resp, err := http.Get("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip")
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch top1m dataset: %s", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read body: %s", err)
+	}
+
+	zipReader, err := zip.NewReader(bytes.NewReader(body), (int64)(len(body)))
+	if err != nil {
+		return nil, fmt.Errorf("failed to unzip body: %s", err)
+	}
+
+	for _, f := range zipReader.File {
+		if f.Name != "top-1m.csv" {
+			return nil, fmt.Errorf("unexpected zip file entry '%s'", f.Name)
+		}
+
+		top1m, err := f.Open()
+		if err != nil {
+			return nil, fmt.Errorf("failed to extract '%s': %s", f.Name, err)
+		}
+
+		csvReader := csv.NewReader(top1m)
+
+		records := make([]string, 0, 1000000)
+		for {
+			// Read CSV records, expecting 2 fields, e.g. "1,google.com"
+			r, err := csvReader.Read()
+			switch err {
+			case nil:
+				records = append(records, r[1])
+			case io.EOF:
+				return records, nil
+
+			default:
+				return nil, fmt.Errorf("bad CSV record: %s", err)
+			}
+
+		}
+	}
+
+	return nil, errors.New("empty zip file")
+}
+
+func fetch(str string, ch chan<- string) {
+	url, err := url.Parse(str)
+	if err != nil {
+		ch <- fmt.Sprintf("failed to parse URL '%s': %s", str, err)
+		return
+	}
+
+	if url.Scheme == "" {
+		url.Scheme = "http"
+	}
+
 	start := time.Now()
-	resp, err := http.Get(url)
+	resp, err := http.Get(url.String())
 	if err != nil {
 		ch <- fmt.Sprint(err) // send to channel ch
 		return
